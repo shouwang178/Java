c73f59066b83d5e15a0635a5e42d43caa2f96590
weixiao 00347190 <weixiao3@huawei.com>
2020-04-05 20:51:44 +0800

    提交==、！=引用和内容对比代码

---
+++ Equivalence/Equivalence.java
@@ -0,0 +1,11 @@
+// operators/Equivalence.java
+public class Equivalence {
+    public static void main(String[] args) {
+        Integer n1 = 47;
+        Integer n2 = 47;
+		//Integer n1 = 128;
+        //Integer n2 = 128;
+        System.out.println(n1 == n2);
+        System.out.println(n1 != n2);
+    }
+}
